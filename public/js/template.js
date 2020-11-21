2eb33ce4b847b004f74a186b5cfe05af5f73c25a
Uxa9 <kirill.yu99@gmail.com>
2020-11-21 10:10:20 +1000

    mobile menu

--- public/js/template.js
+++ public/js/template.js
@@ -1,7 +1,10 @@
 let
     searchBar = document.getElementById('searchWindow'),
     searchButton = document.querySelectorAll('.search'),
-    crossClose = document.getElementById('crossClose');
+    crossClose = document.getElementById('crossClose'),
+    mobileMenu = document.querySelector('.mobile-menu'),
+    burger = mobileMenu.querySelector('.menu'),
+    nav = mobileMenu.querySelector('.navigation');
 
 searchButton.forEach((item, i) => {
     item.onclick = function() {
@@ -13,4 +16,18 @@ searchButton.forEach((item, i) => {
 crossClose.onclick = function() {
     searchBar.style.display = 'none';
     document.body.classList.remove('stop-scrolling');
+}
+
+burger.onclick = function() {
+    if (!burger.classList.contains('open')) {
+        burger.classList.add('open');
+        document.body.classList.add('stop-scrolling');
+        nav.style.transform = "translate(0px, calc(0vh + 150px))";
+    }
+    else {
+        burger.classList.remove('open');
+        document.body.classList.remove('stop-scrolling');
+        document.body.classList.remove('stop-scrolling');
+        nav.style.transform = "translate(0px, 100vh)";
+    }
 }
